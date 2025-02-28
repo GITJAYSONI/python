50f23ebc18b6877bbeb34b45370aa79da568a928
GITJAYSONI <jaysoni43799@gmail.com>
2025-02-28 13:22:17 +0530

    Add files via upload

---
+++ if elsi.py
@@ -0,0 +1,27 @@
+"""number= int(input("enter the number="))
+if(number%2==0):
+  print("the number is even")
+else:
+  print("the number is odd")"""
+
+"""
+
+"""
+"""number=int(input("enter the number="))
+if(number%2==0):
+  print("number divisable by 2")
+elif(number%5==0):
+  print("number is diviable by 5")
+else:
+  print("number is not divi")"""
+
+a =int(input("enter the first number(a):" ))
+b=int(input("enter the second number(b):"))
+c=int(input("enter the third number(c):"))
+if(a<b and a<c):
+  print("a is largest")
+elif(b<a and b<c):
+  print("b is largest")
+else:
+  print ("c is largest")
+  
